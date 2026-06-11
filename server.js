c6939406960347317dc7c2deb710d7b11b27bd0d
KAVI <kavi@localhost>
2026-06-11 13:06:19 +0530

    real server restored

--- server.js
+++ server.js
@@ -77,4 +77,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => console.log('KAVI on port', PORT));
+app.listen(PORT, () => console.log('KAVI on port', PORT, 'Key set:', !!GROQ_KEY));
